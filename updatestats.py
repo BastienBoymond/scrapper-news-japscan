f7176ba1ea666cb8b89547c12ac7023233ac7e09
Bastien <bastien.boymond@epitech.eu>
2023-02-15 15:30:40 +0100

    quick fix

--- updatestats.py
+++ updatestats.py
@@ -6,19 +6,20 @@ class Stats:
         content = self.db.get('japscan-chapter')
         usersStats = self.db.get('stats')
         for user in usersStats:
+            genres_read = []
             for manga in content:
                 if manga['manga_name'] in user['mangalist']:
                     for genre in manga['genres']:
                         genre_exist = False
-                        if user['genres_read'] == []:
-                            user['genres_read'].append({'genre': genre, 'nb': 1})
+                        if genres_read == []:
+                            genres_read.append({'genre': genre, 'nb': 1})
                             continue
-                        for obj in user['genres_read']:
+                        for obj in genres_read:
                             if genre in obj['genre']:
                                 genre_exist = True
                                 obj['nb'] += 1
                         if not genre_exist:
-                            user['genres_read'].append({'genre': genre, 'nb': 1})
-                    self.db.update('stats', "id" ,user['id'], {'genres_read': user['genres_read']})
+                            genres_read.append({'genre': genre, 'nb': 1})
+            self.db.update('stats', "id" ,user['id'], {'genres_read': genres_read})
 
 # [{genre: Action, nb:1}, {genre: Romance, nb:1}]
